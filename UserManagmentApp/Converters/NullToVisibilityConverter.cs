0d527e18d4333e9b1e796a0a054460e8adacb25d
Juan Daza <jc.dazam1@uniandes.edu.co>
2024-04-22 11:53:23 -0500

    Generar el modelo static para Areas

---
+++ UserManagmentApp/Converters/NullToVisibilityConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace UserManagmentApp.Converters
+{
+    public class NullToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Convierte un valor nulo en Visibility.Collapsed y cualquier otro valor en Visibility.Visible
+            return value == null ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
