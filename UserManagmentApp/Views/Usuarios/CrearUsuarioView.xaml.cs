ff79a6168d43f348934ce01436bda072f8edd4f7
Juan Daza <jc.dazam1@uniandes.edu.co>
2024-04-21 21:12:36 -0500

    Agregar el View de Crear Usuario

---
+++ UserManagmentApp/Views/Usuarios/CrearUsuarioView.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using UserManagmentApp.Models;
+
+namespace UserManagmentApp.Views.Usuarios
+{
+    /// <summary>
+    /// Lógica de interacción para CrearUsuarioView.xaml
+    /// </summary>
+    public partial class CrearUsuarioView : UserControl
+    {
+        public CrearUsuarioView()
+        {
+            InitializeComponent();
+        }
+
+        private void Guardar_Click(object sender, RoutedEventArgs e)
+        {
+            // Captura los datos ingresados por el usuario
+            string nombre = txtNombre.Text;
+            string apellido = txtApellido.Text;
+            string documento = txtDocumento.Text;
+            string correo = txtCorreo.Text;
+
+            // Crea un nuevo objeto Usuario con los datos capturados
+            Usuario nuevoUsuario = new Usuario(nombre, apellido, documento, correo);
+        }
+
+        private void Cancelar_Click(object sender, RoutedEventArgs e)
+        {
+            // Obtiene la ventana que contiene este UserControl
+            Window parentWindow = Window.GetWindow(this);
+
+            // Cierra la ventana padre
+            if (parentWindow != null)
+            {
+                parentWindow.Close();
+            }
+        }
+    }
+}
---
+++ UserManagmentApp/Views/Usuarios/EditarUsuarioView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UserManagmentApp.Views.Usuarios
+{
+    /// <summary>
+    /// Lógica de interacción para EditarUsuarioView.xaml
+    /// </summary>
+    public partial class EditarUsuarioView : UserControl
+    {
+        public EditarUsuarioView()
+        {
+            InitializeComponent();
+        }
+    }
+}
